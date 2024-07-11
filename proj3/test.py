9b696b0f4376581e93eef6ef6b2d20754edfcab0
jelfixee <mvlad173173@gmail.com>
2024-07-11 10:31:48 +0300

    Add files via upload

--- proj3/test.py
+++ proj3/test.py
@@ -1,4 +1,3 @@
-import requests
 import aiohttp
 from bs4 import BeautifulSoup
 from time import time
@@ -6,21 +5,87 @@ import json
 import asyncio
 from fake_useragent import UserAgent
 
-def get_dictionary(url, headers):
-    response = requests.get(url=url, headers=headers)
-    soup = BeautifulSoup(response.text, "lxml")
+SEMAPHORE = asyncio.Semaphore(1)
 
-    categories_urls = [[subcat["href"] for subcat in cat.find_all("a")] for cat in soup.find("div", class_="customScrollbar scrollblock scrollblock--thick").find("ul").find_all("ul")]
-    categories_names = [cat.text for cat in [cat_name.find("span", class_="name option-font-bold") for cat_name in soup.find("div", class_="customScrollbar scrollblock scrollblock--thick").find("ul").find_all("li")] if cat]
-    dictionary = {k: v for k, v in zip(categories_names, categories_urls)}
-    return dictionary
+async def fetch(session, url, headers):
 
-def get_page(link, url, headers):
-    return await pagination
+    async with SEMAPHORE:
+        try:
 
-get product_data():
+            async with session.get(url=url, headers=headers) as response:
+                return await response.text()
+
+        except aiohttp.ClientError as e:
+
+            print(f"Ошибка запроса {e}")
+            return None
+
+async def parse_catalogs(html, url):
+
+    soup = BeautifulSoup(html, "lxml")
+
+    categories_urls = []
+
+    for cat_url in soup.find("div", class_="customScrollbar scrollblock scrollblock--thick").find("ul", class_="menu-wrapper menu-type-1").find_all("li"):
+
+        if cat_url.find("ul"):
+
+            for cat in cat_url.find("ul"):
+
+                try:
+                    categories_urls.append(url + cat.find("a")["href"])
+                except:
+                    continue
+
+    return categories_urls
+
+async def get_pagination(html):
+
+    soup = BeautifulSoup(html, "lxml")
+
+    pagination = int(soup.find("div", class_="nums").find_all("a")[-1].text) if soup.find("div", class_="nums") else 0
+
+    return pagination
+
+async def parse_products(html, url):
+
+    soup = BeautifulSoup(html, "lxml")
+
+    products_list = []
+
+    products = soup.find("div", class_="top_wrapper items_wrapper catalog_block_template").find_all("div", class_="col-lg-3 col-md-4 col-sm-6 col-xs-6 col-xxs-12 item item-parent catalog-block-view__item js-notice-block item_block")
+
+    for product in products:
+        try:
+            name = product.find("div", class_="item-title").find("a").text.strip()
+        except:
+            name = "Нет названия"
+        try:
+            product_url = product.find("div", class_="item-title").find("a")["href"]
+        except:
+            product_url = "Нет ссылки"
+        try:
+            price = int("".join([i for i in product.find("div", class_="price_value_block values_wrapper").find("span", class_="price_value").text.strip() if i.isdigit()]))
+        except:
+            price = "Нет цены"
+
+        products_list.append(
+            {
+                "name": name,
+                "price": price,
+                "product_url": url + product_url
+            }
+        )
+    return products_list
+
+
+async def save_json(data):
+
+    with open(r"D:\testsforpy\test3async.json", "w+", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 async def main():
+
     link = "https://latuno.ru"
 
     headers = {
@@ -30,14 +95,40 @@ async def main():
     }
 
     start_time = time()
-    tasks = []
-    dictionary = get_dictionary(link, headers)
 
-    for category in dictionary.keys():
-        for urls in category:
-            tasks.append(asyncio.create_task())
-    await asyncio.gather(*tasks)
+    async with aiohttp.ClientSession() as session:
 
+        html = await fetch(session, link, headers)
+
+        if html is None:
+            return
+
+        catalogs = await parse_catalogs(html, link)
+
+        products_data = []
+
+        for catalog in catalogs:
+
+            html = await fetch(session, catalog, headers)
+
+            if html is None:
+                continue
+
+            pagination = await get_pagination(html)
+
+            tasks = [asyncio.create_task(fetch(session, catalog + f"?PAGEN_1={page}", headers)) for page in range(1, pagination + 1)]
+
+            pages = await asyncio.gather(*tasks)
+
+            for page in pages:
+
+                if page is None:
+                    continue
+
+                products = await parse_products(page, link)
+                products_data.extend(products)
+
+        await save_json(products_data)
 
     print(time() - start_time)
 
